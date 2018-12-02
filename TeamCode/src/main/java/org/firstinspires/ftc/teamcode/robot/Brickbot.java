0645bf43689daaa7aefad4de3a0581ddd21cfefc
LegoInfinity <manole01george@gmail.com>
2018-12-03 00:15:48 +0200

    Removed unused import statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Brickbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Brickbot.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.robot;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @SuppressWarnings("All")
