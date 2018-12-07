0b442b6f00235cb039d35616260e3961709ec775
rty765 <razvancristian12@gmail.com>
2018-12-07 13:23:33 +0200

    Fixed tfod

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TFOreIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TFOreIdentification.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -63,7 +64,8 @@ public class TFOreIdentification {
 		TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
 
 		tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-		tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+		//tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+		tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_SILVER_MINERAL, LABEL_GOLD_MINERAL);
 	}
 
 	public void activate() {
@@ -75,23 +77,42 @@ public class TFOreIdentification {
 	}
 
 	public String getGoldOrePosition() {
-		List<Recognition> recognitions = tfod.getUpdatedRecognitions();
+		List<Recognition> recognitions = null;
 
 		int goldX = -1;
 		int silver1X = -1;
 		int silver2X = -1;
 
-		for (Recognition recognition : recognitions) {
-			if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
-				goldX = (int) recognition.getLeft();
-			} else if (silver1X == -1) {
-				silver1X = (int) recognition.getLeft();
-			} else {
-				silver2X = (int) recognition.getLeft();
+
+		while (goldX == -1) {
+			recognitions = tfod.getUpdatedRecognitions();
+
+			if (recognitions != null) {
+				if (recognitions.size() == 3) {
+
+					for (Recognition recognition : recognitions) {
+						if (goldX == -1 ) {    //recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+							goldX = (int) recognition.getLeft();
+						} else if (silver1X == -1) {
+							silver1X = (int) recognition.getLeft();
+						} else {
+							silver2X = (int) recognition.getLeft();
+						}
+					}
+				}
 			}
 		}
 
 		DistanceUnit unitINCH = DistanceUnit.INCH;
+
+		telemetry.addData("gold", Integer.toString(goldX));
+		telemetry.addData("silver1", Integer.toString(silver1X));
+		telemetry.addData("silver2", Integer.toString(silver2X));
+		telemetry.update();
+
+		ElapsedTime time = new ElapsedTime();
+		time.reset();
+		while (time.seconds() < 3) {}
 
 		//LEFT
 		if (goldX < silver1X && goldX < silver2X) {
